a99e371f6b9f5ebfbf8d037c217878b046ad1a73
Jacob Floisand <jacobfloisand@gmail.com>
2026-04-14 19:25:57 -0600

    Add basic code for Warehouse web app

---
+++ BuildingSuppiesWebApp/BuildingSuppiesWebApp/Pages/Products.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BuildingSuppiesWebApp.Pages
+{
+    public class ProductModel : PageModel
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductModel(IHttpClientFactory factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
+
+        [BindProperty]
+        public string Search { get; set; }
+
+        public List<ProductDto> Results { get; set; } = new();
+
+        public async Task OnPost()
+        {
+            var url = $"https://api.casestudy.enterbridge.com/api/products?name={Search}&PageSize=10";
+
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                var data = JsonSerializer.Deserialize<ProductResponse>(json, options);
+
+                Results = data?.Items ?? new List<ProductDto>();
+            }
+        }
+    }
+
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Sku { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class ProductResponse
+    {
+        public List<ProductDto> Items { get; set; }
+    }
+}
--- REAME.md
+++ REAME.md
@@ -1,7 +1,9 @@
 Contains a repository for a case study warehouse data/orders web UI
 
 AI Tools:
-Normally I'd use Codex, but since I didn't have access to codex on my personal machine, I used chatgpt, etc.
+Normally I'd use Codex (Which could generate the app based on the swagger json in seconds...), but since I didn't have access to codex on my personal machine, I used chatgpt, etc.
+
+ChatGPT helped refresh my memory on the difference between razor pages and MVC.
 
 
 
@@ -10,4 +12,24 @@ Resources used:
 Swagger: https://api.casestudy.enterbridge.com/swagger/index.html
 
 api endpoints: https://api.casestudy.enterbridge.com/
+
+
+
+Design choices:
+
+.NET 6 - I know it's older... but it's what I have on my home computer. In real life I would use the latest (.NET 10)
+
+
+
+Architecture:
+
+A page for product catalog with prices 
+
+A page to view the price history for a product
+
+A page to create an order (Should include templates)
+
+A page to view/edit/delete orders (Communicate edits to the buyer - auto email from business domain?)
+
+
 
